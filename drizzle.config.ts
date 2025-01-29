216ccd7bbd6f5ea1e1c78f4d85e04aea339d76ab
yash91989201 <yashraj6370@gmail.com>
2025-01-29 14:52:06 +0530

    update: use drizzle-kit migrate for db migrations instead of migrate.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,22 @@
-import { type Config } from "drizzle-kit";
+import { z } from "zod";
+import { createEnv } from "@t3-oss/env-nextjs";
+// TYPES
+import type { Config } from "drizzle-kit";
 
-import { env } from "@/env";
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string().url(),
+  },
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  emptyStringAsUndefined: true,
+});
 
 export default {
   schema: "./src/server/db/schema.ts",
+  out: "./src/server/db/migrations",
   dialect: "mysql",
   dbCredentials: {
     url: env.DATABASE_URL,
